8e580965400873bdf6585dbee744aa9c0b44ccb2
Ricardo Garcia <ricardo@pixe.com.mx>
2012-08-24 15:50:24 -0500

    Create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cotiplus::Application.config.secret_token = '8abb2ae465e81f1e1cfdb6295b14082d8882aad240449fb8590977eab1d55fa0708713a427788a9c00d78a69f7a3528a5001f16b3de3e9687959195cd113d97b'
